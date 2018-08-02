6c2024249a0f1ccbb2bab53076615102c591d02d
Felix Khafizov N <asssama212@gmail.com>
2018-08-02 21:13:16 +0300

    Finished features slider

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("click", e => {
+  var targ = e.target;
+
+  if (targ.id.includes("tr")) {
+    console.log("a");
+    var trValue = parseInt(targ.id.slice(2)) * 500;
+    var slides = document.querySelectorAll(".features__slide");
+
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.transform = `translateY(-${trValue}px)`;
+    }
+
+    document
+      .querySelector(".features__trigger--active")
+      .classList.remove("features__trigger--active");
+    targ.classList.add("features__trigger--active");
+  }
+});
