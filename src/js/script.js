0b6f9e301008c23338c8b4983fc719329ad320a6
Felix Khafizov N <asssama212@gmail.com>
2018-08-13 11:16:49 +0300

    Finished the info block

--- src/js/script.js
+++ src/js/script.js
@@ -81,3 +81,80 @@ document.addEventListener("click", e => {
 });
 
 //Third slider (info)
+//1. Define elements
+const cnsBtnPrev = document.querySelector(".cn-slider__arr--left");
+const cnsBtnNext = document.querySelector(".cn-slider__arr--right");
+const cnsSlideWrapper = document.querySelector(".cn-slider__slides-wrapper");
+const cnsItems = document.querySelectorAll(".cn-slider__slide");
+const cnsBgWrapper = document.querySelector(".cn-slider__square-bg-wrapper");
+const cnsControls = document.querySelector(".cn-slider__bullets");
+
+let cnsSlideIndex = 0;
+
+const cnsInitSlider = () => {
+  let totalWidth = cnsBgWrapper.offsetWidth * cnsItems.length;
+  cnsSlideWrapper.style.width = totalWidth + "px";
+};
+
+const cnsInitControls = () => {
+  let buttons = [];
+
+  for (let i = 0; i < cnsItems.length; i++) {
+    let button = document.createElement("button");
+    button.textContent = i < 9 ? `0${i + 1}` : `${i + 1}`;
+    button.className = "cn-slider__control";
+    button.setAttribute("data-cnslider-index", i);
+    if (i === 0) {
+      button.classList.add("cn-slider__control--active");
+    }
+    buttons.push(button);
+
+    buttons.forEach(button => {
+      cnsControls.appendChild(button);
+    });
+  }
+
+  cnsCheckButtons();
+};
+
+const cnsMoveSlider = () => {
+  cnsSlideWrapper.style.transform = `translateX(-${cnsBgWrapper.offsetWidth *
+    cnsSlideIndex}px)`;
+  document
+    .querySelector(".cn-slider__control--active")
+    .classList.remove("cn-slider__control--active");
+  let controls = document.querySelectorAll(".cn-slider__control");
+  controls[cnsSlideIndex].classList.add("cn-slider__control--active");
+};
+
+const cnsCheckButtons = () => {
+  if (cnsSlideIndex === 0) {
+    cnsBtnPrev.setAttribute("disabled", "true");
+  }
+  if (cnsSlideIndex > 0) {
+    cnsBtnPrev.removeAttribute("disabled");
+  }
+  if (cnsSlideIndex === cnsItems.length - 1) {
+    cnsBtnNext.setAttribute("disabled", "true");
+  }
+  if (cnsSlideIndex < cnsItems.length - 1) {
+    cnsBtnNext.removeAttribute("disabled");
+  }
+};
+
+document.addEventListener("click", e => {
+  let targ = e.target;
+  if (targ.className.includes("cn-slider__arr--left")) {
+    cnsSlideIndex--;
+  } else if (targ.className.includes("cn-slider__arr--right")) {
+    cnsSlideIndex++;
+  } else if (targ.className.includes("cn-slider__control")) {
+    cnsSlideIndex = parseInt(targ.getAttribute("data-cnslider-index"));
+  }
+
+  cnsMoveSlider();
+  cnsCheckButtons();
+});
+
+cnsInitSlider();
+cnsInitControls();
